423fc1d9e36044a1d1f54d358309e1963b59b92f
003allstar <46111743+003allstar@users.noreply.github.com>
2019-01-16 23:22:19 -0500

    Add files via upload
    Adding the game.

---
+++ The guessing game.py
@@ -0,0 +1,23 @@
+import random
+
+high_number = 30
+answer = random.randint(1, high_number)
+times_run = 0
+guess = int(input('I am thinking of a number between 1-30.'
+                  'What is your guess:'))
+
+while times_run < 4:
+    if guess != answer:
+        if guess < answer:
+            print('Your guess is too low. Guess higher.')
+            guess = int(input('What is your next guess:'))
+            times_run += 1
+        else:
+            print('Your guess is too high. Guess lower.')
+            guess = int(input('What is your next guess:'))
+            times_run += 1
+    else:
+        print('You got it.')
+        break
+else:
+    print("You didn't get it. The number is {}.".format(answer))
